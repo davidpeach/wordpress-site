e463f161ace92b94a0ef8dbfc2b27f78379d634e
David Peach <mail@davidpeach.co.uk>
2020-02-11 20:32:07 +0000

    Add Book fields and disalow from front end search.

--- web/app/themes/shiver/includes/post-types/books.php
+++ web/app/themes/shiver/includes/post-types/books.php
@@ -19,8 +19,8 @@ function shiver_register_post_type_cpts_books() {
 		"label" => __( "Books", "shiver" ),
 		"labels" => $labels,
 		"description" => "Keeping track of the books I have read.",
-		"public" => true,
-		"publicly_queryable" => true,
+		"public" => false,
+		"publicly_queryable" => false,
 		"show_ui" => true,
 		"show_in_rest" => true,
 		"rest_base" => "",
@@ -29,7 +29,7 @@ function shiver_register_post_type_cpts_books() {
 		"show_in_menu" => true,
 		"show_in_nav_menus" => true,
 		"delete_with_user" => false,
-		"exclude_from_search" => false,
+		"exclude_from_search" => true,
 		"capability_type" => "post",
 		"map_meta_cap" => true,
 		"hierarchical" => false,
@@ -43,3 +43,85 @@ function shiver_register_post_type_cpts_books() {
 }
 
 add_action( 'init', 'shiver_register_post_type_cpts_books' );
+
+function shiver_register_acf_group_book_fields(): void
+{
+	if( function_exists('acf_add_local_field_group') ):
+
+		acf_add_local_field_group(array(
+			'key' => 'group_5e430ddecd1ba',
+			'title' => 'Book Fields',
+			'fields' => array(
+				array(
+					'key' => 'field_5e430de1c700f',
+					'label' => 'Readable Ting',
+					'name' => 'readable_ting',
+					'type' => 'relationship',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'post_type' => array(
+						0 => 'post',
+					),
+					'taxonomy' => '',
+					'filters' => array(
+						0 => 'search',
+						1 => 'post_type',
+						2 => 'taxonomy',
+					),
+					'elements' => array(
+						0 => 'featured_image',
+					),
+					'min' => '',
+					'max' => '',
+					'return_format' => 'object',
+				),
+				array(
+					'key' => 'field_5e430e52abf17',
+					'label' => 'Number Of Pages',
+					'name' => 'number_of_pages',
+					'type' => 'number',
+					'instructions' => 'How many pages does this book have?',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'min' => '',
+					'max' => '',
+					'step' => '',
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'books',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => true,
+			'description' => '',
+		));
+
+	endif;
+}
+add_action( 'init', 'shiver_register_acf_group_book_fields' );
