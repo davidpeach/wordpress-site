7a9cae634a7df35a28d4ca19d52906869eb35b25
David Peach <mail@davidpeach.co.uk>
2020-04-05 21:50:16 +0100

    Style update to archive intro content.

--- web/app/themes/shiver/parts/archives/content.php
+++ web/app/themes/shiver/parts/archives/content.php
@@ -1,7 +1,9 @@
 <?php if ($content = get_field('content', get_queried_object())): ?>
 <div class="section-inner">
 	<div class="entry-content post-inner">
-		<?php echo $content; ?>
+		<div class="archive-content__text">
+			<?php echo $content; ?>
+		</div>
 	</div>
 </div>
 <?php endif; ?>
